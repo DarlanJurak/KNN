dccb6778273885c115eea21ed61835d144452702
DarlanJurak <darlan.jurak@gmail.com>
2018-04-15 11:52:23 -0300

    Added Instance class that is going to replace Sample class

--- makefile
+++ makefile
@@ -7,16 +7,16 @@ CXXFLAGS	=	-std=c++11
 
 
 #Generates KNN exectuble file unifying class and main function object codes
-knn: knn.o Sample.o
-	$(CXX) knn.o Sample.o -o knn
+knn: knn.o Instance.o
+	$(CXX) knn.o Instance.o -o knn
 
 #Generates KNN (main function) object code
 knn.o: src/knn.cpp
 	$(CXX) -c $(CXXFLAGS) src/knn.cpp
 
-#Generates Sample (class) object code
-Sample.o: src/Sample.cpp inc/Sample.hpp
-	$(CXX) -c src/Sample.cpp
+#Generates Instance (class) object code
+Instance.o: src/Instance.cpp inc/Instance.hpp
+	$(CXX) -c src/Instance.cpp
 
 clean:
 	$(RM) *.o knn
--- src/knn.cpp
+++ src/knn.cpp
@@ -1,5 +1,5 @@
 
-#include "../inc/Sample.hpp"    // Sample class.
+#include "../inc/Instance.hpp"    // Instance class.
 #include <iostream>             // cout and cin.
 #include <string>               // String.
 #include <boost/algorithm/string.hpp>   // String split.
@@ -8,13 +8,13 @@ using namespace std;
 
 int main() {
 
-    vector<Sample *> samples;           // Data structure to store samples.
-    vector<Sample *> new_instances;     // Data structure to store samples.
+    vector<Instance *> samples;           // Data structure to store samples.
+    vector<Instance *> new_instances;     // Data structure to store new instances.
     string user_input;                  // Variable to interact with user.
-    vector<float> instance_input_float;         // Intermediary attributes vector.
+    vector<float>       instance_input_float;  // Intermediary attributes vector.
     vector<std::string> instance_input_string; // Used in the get line split.
 
-    // User interaction. Asks for Samples attributes.
+    // User interaction. Asks for Instances attributes.
     cout << "Please, insert the training base respecting the following rules: \n"
         << "* Insert numbers only (they CAN be float) \n"
         << "* The FIRST number in a line represents a CLASS \n"
@@ -28,7 +28,7 @@ int main() {
 
     getline(cin, user_input);   // Get first user input.
 
-    // While receives input, set them as Samples's attributes.
+    // While receives input, set them as Instances's attributes.
     while(user_input.size() > 0){
 
         // Splits input string by <space>.
@@ -41,36 +41,37 @@ int main() {
             return std::stof(val);
         });
 
-        // Creates new Sample instance with class and attributes.
+        // Creates new Instance instance with class and attributes.
         int category = (int)*(instance_input_float.begin());        // Gets class information.
         instance_input_float.erase(instance_input_float.begin());   // Removes class for vector reuse.
-        samples.push_back(new Sample(category, instance_input_float));  // Creates new sample.
+        samples.push_back(new Instance(category, instance_input_float));  // Creates new sample.
 
         getline(cin, user_input);   // Get user input (new attributes set).
     }
 
     // Shows samples and their attributes
     int count = 0;  // Counter to help user. (Helps to see the ordered list of samples).
-    for (std::vector<Sample *>::iterator it = samples.begin(); it != samples.end(); ++it){
+    for (std::vector<Instance *>::iterator it = samples.begin(); it != samples.end(); ++it){
 
-        std::cout << "Sample " <<  count++ << ": "
+        std::cout << "Instance " <<  count++ << ": "
             << "class: " << (*it)->getCategory() << " "
             << "attributes: ";
         (*it)->showAttributes();
 
     }
 
-    // Instance to be classified input
-    cout << "\n Now please, insert the instance to be classified. Remember:\n"
-        << "* Insert numbers only (they CAN be float) \n"
-        << "* Finish insertion by entering <enter> \n"
-        << "Example:   \n"
-        << "1.2 3.6      \n" << endl;
 
-    getline(cin, user_input);   // Get user input. (Idealy the new object's attributes).
-
-    // Splits input string by <space>.
-    boost::split(instance_input_string, user_input, [](char c){return c == ' ';});
+    // // Instance to be classified input
+    // cout << "\n Now please, insert the instance to be classified. Remember:\n"
+    //     << "* Insert numbers only (they CAN be float) \n"
+    //     << "* Finish insertion by entering <enter> \n"
+    //     << "Example:   \n"
+    //     << "1.2 3.6      \n" << endl;
+    //
+    // getline(cin, user_input);   // Get user input. (Idealy the new object's attributes).
+    //
+    // // Splits input string by <space>.
+    // boost::split(instance_input_string, user_input, [](char c){return c == ' ';});
 
     // // Transforms from string vector to float vector.
     // transform(instance_input_string.begin(), instance_input_string.end(), instance_input_float.begin(), [](const std::string& val)
@@ -80,20 +81,20 @@ int main() {
     //
     // std::cout << "\n\nDEBUG 1 \n\n\n" << '\n';
     //
-    // // Creates new Sample instance with class and attributes.
-    // new_instances.push_back(new Sample(NULL, instance_input_float));  // Creates new sample.
+    // // Creates new Instance instance with class and attributes.
+    // new_instances.push_back(new Instance(NULL, instance_input_float));  // Creates new sample.
     //
     // // calculates Similarity
-    // for (std::vector<Sample *>::iterator it = samples.begin(); it != samples.end(); ++it){
+    // for (std::vector<Instance *>::iterator it = samples.begin(); it != samples.end(); ++it){
     //
     //     (*it)->calculateSimilarityByEuclidianDistance((*(new_instances.begin()))->attributes);
     //
     // }
     //
     // count = 0;
-    // for (std::vector<Sample *>::iterator it = samples.begin(); it != samples.end(); ++it){
+    // for (std::vector<Instance *>::iterator it = samples.begin(); it != samples.end(); ++it){
     //
-    //     std::cout << "Sample " << count++ << ". Similarity: " << (*it)->getSimilarity() << "." << endl;
+    //     std::cout << "Instance " << count++ << ". Similarity: " << (*it)->getSimilarity() << "." << endl;
     //
     // }
 
